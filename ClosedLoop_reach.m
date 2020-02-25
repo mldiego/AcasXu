2307063bf3af9afb5c23cf460969afac86ae45a6
manzand <diego.manzanas.lopez@vanderbilt.edu>
2020-02-25 13:38:39 -0600

    Finished Simulink closedloop

---
+++ ClosedLoop_reach.m
@@ -0,0 +1,7 @@
+%% Reachability analysis of the closedLoop system of AcasXu with Dubins model
+% There are 2 main components required:
+% - Dubins plant x2 (ownship and intruder)
+% - AcasXu networks x45 (choose based on t and previous advisory)
+
+ownship = NonLinearODE(3,1,@dubindynamics);
+intruder = NonLinearODE(3,1,@dubindynamics);
