c2fea91417b9c3a4f2143131e46167481d1e49cd
dieman95 <diego.manzanas.lopez@vanderbilt.edu>
2020-05-25 11:29:08 -0500

    Update MATLAB deep learning format to onnx

--- code/functions/ToONNX.m
+++ code/functions/ToONNX.m
@@ -14,21 +14,44 @@ lgraph = addLayers(lgraph,lin);
 n = length(W);
 for i=1:n
     a = split(string(act_fcns(i,:)));
+%     disp(a);
     if strcmp(a(1),"relu") % Check activation function in layer
         layer = [fullyConnectedLayer(size(W{i},1),'Name','Operation_'+string(i)) 
         reluLayer('Name','relu_'+string(i))];
+        lgraph = addLayers(lgraph,layer);
 %         disp('Relu layer')
     elseif strcmp(a(1),"linear")  % Check activation function in layer
-        layer = fullyConnectedLayer(size(W{i},1),'Name','linear_'+string(i)); 
+        layer = fullyConnectedLayer(size(W{i},1),'Name','linear_'+string(i));
+        lgraph = addLayers(lgraph,layer);
 %         disp('Linear layer')
+    elseif strcmp(a(1),"tansig") || strcmp(a(1),"tanh")
+%         layer = [fullyConnectedLayer(size(W{i},1),'Name','Operation_'+string(i)) 
+%         tanhLayer('Name','tanh_'+string(i))];
+        layer = [fullyConnectedLayer(size(W{i},1),'Name','Operation_'+string(i)) 
+        nnet.keras.layer.TanhLayer('tanh_'+string(i))];
+        lgraph = addLayers(lgraph,layer);
+    elseif strcmp(a(1),"sigmoid") || strcmp(a(1),"logsig")
+        layer = [fullyConnectedLayer(size(W{i},1),'Name','Operation_'+string(i)) 
+        nnet.keras.layer.SigmoidLayer('sigmoid_'+string(i))];
+        lgraph = addLayers(lgraph,layer);
+    else
+        disp('Unknown hidden layer');
     end
 %     disp(layer)
-    lgraph = addLayers(lgraph,layer);
+%     lgraph = addLayers(lgraph,layer);
 end
-
-% Add output layer
-lout = regressionLayer('Name','output');
+% disp(lgraph);
+% disp(a(1));
+if strcmp(a(1),"softmax")
+     lout = [fullyConnectedLayer(size(W{i},1),'Name','Operation_'+string(i)) 
+        softmaxLayer('Name','softmax_'+string(i))
+        classificationLayer('Name','output')];
+else
+    % Add output layer
+    lout = regressionLayer('Name','output');
+end
 lgraph = addLayers(lgraph,lout);
+% disp(lgraph.Layers);
 
 %% Add weigths and bias values to layers
 newlays = lgraph.Layers;
